7243a733f38abdc07627a3ab47d14e75ebd3f501
tracer-ics2017 <tracer@njuics.org>
2026-04-27 08:52:04 +0000

    >  compile 161220000 ics Linux bab1ac518fbe 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  08:52:04 up  1:06,  0 users,  load average: 0.37, 0.18, 0.06 ae219764e8fe45dc578e1c2a558b0136f1dea222

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,17 +1,20 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov);
-
 make_EHelper(operand_size);
 
 make_EHelper(inv);
+make_EHelper(sub);
+
 make_EHelper(nemu_trap);
 
 make_EHelper(call);
 make_EHelper(ret);
+
+make_EHelper(mov);
 make_EHelper(push);
 make_EHelper(pop);
-make_EHelper(sub);
+make_EHelper(lea);
+
 make_EHelper(xor);
 
 
