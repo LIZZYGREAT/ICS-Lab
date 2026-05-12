c1052c78164b6f18e7899a293e07d9a816292134
tracer-ics2017 <tracer@njuics.org>
2026-05-12 12:28:39 +0000

    >  compile 161220000 ics Linux bab1ac518fbe 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  12:28:39 up  2:46,  0 users,  load average: 0.01, 0.05, 0.01 f0948d84812e50978ae2e1b6e631debe41fa37c5

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,10 +13,27 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
+  /* Check if the physical address falls into any memory-mapped I/O space */
+  int map_NO = is_mmio(addr);
+  if (map_NO != -1) {
+    /* Route the read request to the corresponding MMIO device controller */
+    return mmio_read(addr, len, map_NO);
+  }
+
+  /* Regular physical memory read using the mask code to get the right length */
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-} /* here use the mask code to get the right length of ans */
+}
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
+  /* Check if the physical address falls into any memory-mapped I/O space */
+  int map_NO = is_mmio(addr);
+  if (map_NO != -1) {
+    /* Route the write request to the corresponding MMIO device controller */
+    mmio_write(addr, len, data, map_NO);
+    return;
+  }
+
+  /* Regular physical memory write to the pmem array */
   memcpy(guest_to_host(addr), &data, len);
 }
 
