96c67c688f94fe0ab6e22c18f0d7143bbd4656ab
tracer-ics2017 <tracer@njuics.org>
2026-06-10 06:53:32 +0000

    >  run 161220000 ics Linux bab1ac518fbe 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 GNU/Linux  06:53:32 up  3:29,  0 users,  load average: 0.04, 0.06, 0.01 a4fa2aaf4540105c7b836c67eb2a80cc2d96b25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,10 @@
 #include "common.h"
 #include "syscall.h"
+
 extern void _yield(); 
+
+extern void _putc(char c);
+
 _RegSet* do_syscall(_RegSet *r) {
   // Array to store the syscall number and its up to 3 arguments
   uintptr_t a[4];
@@ -18,7 +22,7 @@ _RegSet* do_syscall(_RegSet *r) {
       r->eax = 1; 
       break;
     case SYS_yield:
-        r->eax = 0;
+      r->eax = 0;
       break;
 
     case SYS_exit:
@@ -26,6 +30,22 @@ _RegSet* do_syscall(_RegSet *r) {
       // a[1] contains the exit code passed by the user program
       _halt(a[1]);
       break;
+
+    case SYS_write: {
+      int fd = (int)a[1];
+      char *buf = (char *)a[2];
+      size_t len = (size_t)a[3];
+
+      if (fd == 1 || fd == 2) {
+        for (size_t i = 0; i < len; i++) {
+          _putc(buf[i]);
+        }
+        r->eax = len;
+      } else {
+        r->eax = -1;
+      }
+      break;
+    }
 
     default:
       panic("Unhandled syscall ID = %d", a[0]);
